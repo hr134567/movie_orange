c82e38e88cbac9b714077e26339516e2c85e6088
master <haram040131@naver.com>
2023-06-01 09:55:37 +0900

    Add get_reviews

--- db/connection.py
+++ db/connection.py
@@ -8,13 +8,29 @@
 # 3. 결과 받기 (DB ----> Python)
 # 4. Connection 끊기 (Python XXXX DB)
 
+# Mongodb 구조
+# - Mongodb(DBMS) : 데이터베이스 관리 시스템
+#   → DB(NAVER) : 프로젝트 단위
+#       ▶ collection (회원) - CRUD
+#       ▶ collection (카페) - CRUD
+#       ▶ collection (쇼핑) - CRUD
+#       ▶ collection (메일) - CRUD
+#   → DB(KAKAO)
+#   → DB(BLOG)
+# Mongodb 데이터 주고 받기
+# - Mongodb는 BSON Type으로 데이터를 주고 받음
+# - BSON(Binarty JSON) = JSON과 거의 동일
+# - 그냥 JSON Type으로 사용하면 됨(문제 없음)
+# - Python에서 JSON은 Dict Type 사용!(Python Dict = JSON)
+
 from pymongo import MongoClient
 
 # MongoDB connetcion
 def conn() :
     client = MongoClient("mongodb+srv://cnu:haram1203@cluster0.btlnzq9.mongodb.net/")
-    #  → IP, Port, ID&PW
-    db = client["movie"]
-
+    #  → mongodb_web : IP, Port, ID&PW
+    # Python-mongodb 연결 -> client
+    db = client["review"]   # DB 선택
+    # collection 선택
     collection = db.get_collection("movie")
     return collection
--- db/movie_crud.py
+++ db/movie_crud.py
@@ -6,8 +6,23 @@
 from db.connection import conn
 # 리뷰 저장(MongoDB)
 def add_review(data) :
-    collection = conn()
-    collection.insert_one(data)        #리뷰 저장
+    collection = conn()  # Conn(DB : review, Collection : movie) 정보
+    # insert_one : 1건 저장(CREATE)
+    collection.insert_one(data)    #리뷰 저장
 # 리뷰 조회(MongoDB)
 def get_reviews() :
-     pass
+     review_list = []    # Mongodb에서 가져온 Data 저장!
+     collection = conn()   # connection 맺기!
+     # find({조건}, {컬럼 선택}) : 데이터 조회
+     # - 조건 예 : 평점 8점 이상 조회
+     # - 컬럼 선택 예 : review, score 조회
+     #    *컬럼(title, review, scroe, writer, regdate)
+     #    *_id(고유한 식별값) 기본으로 조회 설정
+     # find()를 통해서 MongoDB에서 데이터를 가져옴(MongoDB Type)
+     for one in collection.find({}, {"_id" : 0, "title" : 1, "score" : 1, "review" : 1}) :
+         review_list.append([
+             one["title"],
+             one["review"],
+             one["score"]
+         ])
+     return review_list
